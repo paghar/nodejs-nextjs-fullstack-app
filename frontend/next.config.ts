ad578edc5f586c0bb5abc50c64208f5591f162d8
fpaghar <f.paghar@gmail.com>
2025-05-20 13:45:51 +0200

    Clean code

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
  
+  //should be deleted when Backend API is ready
   images: {
     remotePatterns: [new URL("https://loremflickr.com/**")],
   },
