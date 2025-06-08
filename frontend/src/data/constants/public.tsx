2d4775763e4b1f21aed189f129d105a9b9db07b9
fatemeh paghar <66066475+paghar@users.noreply.github.com>
2025-06-08 17:09:39 +0200

    update Backend API URL

--- frontend/src/data/constants/public.tsx
+++ frontend/src/data/constants/public.tsx
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://localhost:3001/";
-export const API_PRODUCT = "http://localhost:3001/api/products";
-export const API_PRODUCT_PAGINATE = "http://localhost:3001/api/products/paginated?";
+export const API_BASE_URL = "https://nodejs-nextjs-fullstack-app.onrender.com/";
+export const API_PRODUCT = "https://nodejs-nextjs-fullstack-app.onrender.com/api/products";
+export const API_PRODUCT_PAGINATE = "https://nodejs-nextjs-fullstack-app.onrender.com/api/products/paginated?";
