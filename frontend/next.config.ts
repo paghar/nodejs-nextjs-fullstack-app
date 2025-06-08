8618f7b7e24bead5d35d8c68f37834416b4aa8b9
fatemeh paghar <66066475+paghar@users.noreply.github.com>
2025-06-08 17:50:52 +0200

    ready to deploy as webservice in render

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,8 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
+  images: {   
     remotePatterns: [
       {
         protocol: "http",
